9954829431162bad409ee0f1e1bd407c833a91d1
heihei <>
2021-12-29 23:22:15 +0900

    データベースのテスト

--- django_app/sns/tests.py
+++ django_app/sns/tests.py
@@ -1,13 +1,26 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from .models import Message
+
 # Create your tests here.
 class SnsTests(TestCase):
+
     def test_check(self):
-        x = True
-        self.assertTrue(x)
-        y = 100
-        self.assertGreater(y, 0)
-        arr = [10, 20, 30]
-        self.assertIn(20, arr)
-        nn = None
-        self.assertIsNone(nn)
+        usr = User.objects.filter(username='test').first()
+
+        msg = Message.objects.filter(content="test").first()
+        self.assertIs(msg.owner_id, usr.id)
+        self.assertEqual(msg.owner.username, usr.username)
+        self.assertEqual(msg.group.title, 'public')
+
+        msgs = Message.objects.filter(content__contains="test").all()
+        self.assertIs(msgs.count(), 2)
+        
+        c = Message.objects.all().count()
+        self.assertIs(c,5)
+        
+        msg1 = Message.objects.all().first()
+        msg2 = Message.objects.all().last()
+        self.assertIsNot(msg1, msg2)
+
